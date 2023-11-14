86e5e6e7104109a3fbb4a1b1b17fca015b3960cd
Rachitsharma772 <148371197+Rachitsharma772@users.noreply.github.com>
2023-11-14 12:32:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+{
+    "token": "",
+    "proxy": false,
+    "nuke": {
+        "channels_name": [
+            "Ronak papa ",
+            "The Secret paradise on fire",
+            "Ronak on top",
+            "RONAK TUMHARA BAAP HAI"
+        ],
+        "roles_name": [
+            "Fucked by ronak ",
+            "The secret paradise",
+            "Ronak  Papa was  Here"
+        ],
+        "messages_content": [
+            "@everyone @here **Ronak  Papa Aye The** https://discord.gg/tsponfire",
+            "@everyone @here **Ronak  Papa Aye The** https://discord.gg/tsponfire",
+            "@everyone @here **Ronak Papa Aye The** https://discord.gg/tsponfire"
+        ]
+    }
+}
